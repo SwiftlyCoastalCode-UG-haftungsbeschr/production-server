23c5b2455f1267395d1e65075a45a85890356725
Nico Petersen <petersen.nico@icloud.com>
2024-04-14 21:48:59 +0200

    get backend cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,8 +9,8 @@ fi
 #domains=(dresspal.de)
 #domains=(www.dresspal.de)
 #domains=(swiftlycoastalcode.de)
-domains=(www.swiftlycoastalcode.de)
-#domains=(dresspal.scc-backend.de)
+#domains=(www.swiftlycoastalcode.de)
+domains=(dresspal.scc-backend.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="swiftlycoastalcode@gmx.de" # Adding a valid address is strongly recommended
